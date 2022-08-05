d537cfb5a4102b7e4ac1c27c99e61047e3d45ad7
Jochen Henneberg <jh@henneberg-systemdesign.com>
2022-08-05 14:53:51 +0200

    dir/svn: Started implementing object like SVN backup

--- src/dir/svn.rs
+++ src/dir/svn.rs
@@ -2,22 +2,68 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+use std::process::{Command, Stdio};
+
+use xml::EventReader;
+use xml::name::OwnedName;
+use xml::reader::XmlEvent;
+use log::{trace};
+
+use super::utils::SyncError;
 use super::{Category, Dir, Flavour};
 
 pub struct Svn {
     dir: Box<Option<Dir>>,
     ignore: bool,
+    full: bool,
+    ignore_unversioned: bool,
+    ignore_modified: bool,
+}
+
+impl Svn {
+    fn dup_all(&self) -> Result<(), SyncError> {
+        trace!("Run svn command to get status");
+        let svn = Command::new("svn")
+            .arg("status")
+            .arg("--xml")
+            .stdout(Stdio::piped())
+            .spawn()?;
+
+        trace!("Parse XML output");
+        let mut reader = EventReader::new(svn.stdout.unwrap());
+        let t = std::thread::spawn(move || loop {
+            if let Ok(e) = reader.next() {
+                match e {
+                    XmlEvent::StartElement{name: OwnedName{local_name: n, ..}, ..} => trace!("Start element found: {}", n),
+                    XmlEvent::EndElement{name: OwnedName{local_name: n, ..}} => trace!("Start element found: {}", n),
+                    XmlEvent::EndDocument => break,
+                    _ => ()
+                }
+            }
+        });
+        trace!("XML parsing done");
+
+        let _ = t.join();
+
+        Ok(())
+    }
 }
 
 impl Flavour for Svn {
     fn init_opts(opts: &mut getopts::Options) {
         opts.optflag("", "svn-ignore", "Ignore Svn repositories");
+        opts.optflag("", "svn-full", "Full backup (default is unversioned and modified)");
+        opts.optflag("", "svn-ignore-unversioned", "Don't backup unversioned files");
+        opts.optflag("", "svn-ignore-modified", "Don't backup modified files");
     }
 
     fn template(args: &getopts::Matches) -> Self {
         Svn {
             dir: Box::new(None),
             ignore: args.opt_present("svn-ignore"),
+            full: args.opt_present("svn-full"),
+            ignore_unversioned: args.opt_present("svn-ignore-unversioned"),
+            ignore_modified: args.opt_present("svn-ignore-modified"),
         }
     }
 
@@ -35,6 +81,9 @@ impl Flavour for Svn {
         Box::new(Svn {
             dir: Box::new(None),
             ignore: self.ignore,
+            full: self.full,
+            ignore_unversioned: self.ignore_unversioned,
+            ignore_modified: self.ignore_modified,
         })
     }
 
@@ -46,13 +95,17 @@ impl Flavour for Svn {
         &*self.dir
     }
 
+    fn name(&self) -> &'static str {
+        "Subversion"
+    }
+
     fn category(&self) -> Category {
         Category::Repository
     }
 
     /// Recurse if --svn-ignore is not set.
     fn recurse(&self) -> bool {
-        !self.skip()
+        self.full
     }
 
     /// Recurse if --svn-ignore is set.
@@ -64,7 +117,27 @@ impl Flavour for Svn {
         false
     }
 
-    fn name(&self) -> &'static str {
-        "Subversion"
+    fn dup(&self) -> Result<(), SyncError> {
+        if !self.full {
+            self.dup_all()
+        } else if let Some(d) = self.dir() {
+            d.dup()
+        } else {
+            Err(SyncError::Failed(
+                "Cannot synchronize without directory".to_string(),
+            ))
+        }
+    }
+
+    fn merge(&self) -> Result<(), SyncError> {
+        if !self.full {
+            self.dup_all()
+        } else if let Some(d) = self.dir() {
+            d.merge()
+        } else {
+            Err(SyncError::Failed(
+                "Cannot synchronize without directory".to_string(),
+            ))
+        }
     }
 }
